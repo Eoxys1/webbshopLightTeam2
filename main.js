4897ade40bf72e51965764372208744f0c7afed8
jackgustafsson <j.gustafsson05@gmail.com>
2024-09-19 15:26:58 +0200

    worked in index and main

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-const item_list = document.getElementById("item_list")
+const item_list = document.getElementById("item_list");
 for (const element of item_list.children) {
-	const item_name = element.getAttribute("item")
-	const add_btn = element.querySelector(".add_item")
-	const remove_btn = element.querySelector(".remove_item")
+	const item_name = element.getAttribute("item");
+	const add_btn = element.querySelector(".add_item");
+	const remove_btn = element.querySelector(".remove_item");
+	let qty = element.querySelector('.qty');
 	add_btn.addEventListener("click", () => {
-		cart.add(item_name)
-	})
+		cart.add(item_name);
+		qty.value++;
+	});
 	remove_btn.addEventListener("click", () => {
-		cart.remove(item_name)
-	})
+		cart.remove(item_name);
+		qty.value--;
+        if (qty.value < 0) {
+            qty.value = 0;
+        }
+	});
 }
